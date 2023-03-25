eca95abd1ca483f153b4117d0334231fbe840b1c
profcpmuniz <121521898+profcpmuniz@users.noreply.github.com>
2023-03-25 09:56:48 +0000

    Post

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def myRootMethod():
+    return "Hello World"
+
+@app.route("/api/person/")
+def getPersons():
+    people = [{"id":1, "name":"Carlos Muniz"},
+    {"id":2, "name":"Bruno"}]
+    return jsonify(people)
+
+
+@app.route("/api/person/<int:id>")
+def getPerson(id):
+    if id == 10:
+        content = {"details": "Hey, there has been an error on your request"}
+        resp = jsonify(content)
+        resp.status_code = 404
+        return resp
+    person = {"id":id, "name":"Carlos Muniz"}
+    return jsonify(person)
+
+@app.route("/api/person/<int:id>", methods=['DELETE'])
+def deletePerson(id):
+    if id == 10:
+        content = {"details": "Hey, there has been an error on your request"}
+        resp = jsonify(content)
+        resp.status_code = 404
+        return resp
+    return '',204
+
+@app.route("/api/person/", methods=['POST'])
+def addPerson():
+    body = request.get_json()
+    resposeBody = {"id":1,"name": body['name']+ " Created!!!"}
+    return jsonify(resposeBody)
+
+
+
+app.run(host='0.0.0.0')
