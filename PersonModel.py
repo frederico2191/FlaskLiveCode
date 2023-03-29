eedc6e640f83711f0bc3c639fc20aa56d230d01b
Frederico Rodrigues <frederico2191@gmail.com>
2023-03-29 11:56:29 +0000

    GET, POST - Films. GET,DELETE,PUT - People

--- PersonModel.py
+++ PersonModel.py
@@ -2,30 +2,19 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class PersonModel(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(250), nullable=False)
-    
-    def serialize(self):
-        return {
-            "id": self.id,
-            "name": self.name
-        }
-
-
 class Films(db.Model):
     __tablename__ = 'films'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(250), nullable=False)
-    planets = db.Column(db.String(2500), nullable=False, default="No info")
-    characters = db.Column(db.String(2500), nullable=True)
-    created = db.Column(db.Integer, nullable=False, default="No info")
-    edited = db.Column(db.Integer, nullable=False, default="No info")
-    producer = db.Column(db.String(30), nullable=False, default="No info")
-    director = db.Column(db.String(30), nullable=False, default="No info")
-    realease_date = db.Column(db.Integer, nullable=False, default="No info")
-    opening_crawl = db.Column(db.String(250), nullable=False, default="No info")
-    url = db.Column(db.String(250), nullable=False, default="No info")
+    planets = db.Column(db.String(2500), nullable=True)
+    people = db.Column(db.String(2500), nullable=True)
+    created = db.Column(db.Integer, nullable=True)
+    edited = db.Column(db.Integer, nullable=True)
+    producer = db.Column(db.String(30), nullable=True)
+    director = db.Column(db.String(30), nullable=True)
+    realease_date = db.Column(db.Integer, nullable=True)
+    opening_crawl = db.Column(db.String(250), nullable=True)
+    url = db.Column(db.String(250), nullable=True)
 
     def serialize(self):
         return {
@@ -33,128 +22,101 @@ class Films(db.Model):
             "title": self.title
         }
 
+class People(db.Model):
+    __tablename__ = 'people'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    hair_color = db.Column(db.String(30), nullable=True)
+    eye_color = db.Column(db.String(30), nullable=True)
+    birth_year = db.Column(db.Integer, nullable=True)
+    gender = db.Column(db.String(250), nullable=True)
+    homeworld = db.Column(db.String(250), nullable=True)
+    def serialize(self):
+        return {
+            "id": self.id,
+            "title": self.name
+        }
+
+
+class Vehicles(db.Model):
+    __tablename__ = 'vehicles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=True)
+    model = db.Column(db.String(2500), nullable=True)
+    vehicle_class = db.Column(db.String(2500), nullable=True)
+    manufacturer = db.Column(db.String(2500), nullable=True)
+    cost_in_credits = db.Column(db.String(2500), nullable=True)
+    length = db.Column(db.Integer, nullable=True)
+    crew = db.Column(db.String(2500), nullable=True)
+    passangers = db.Column(db.Integer, nullable=True)
+    max_atmosphering_speed = db.Column(db.Integer, nullable=True)
+    cargo_capacity = db.Column(db.Integer, nullable=True)
+    max_atmosphering_speed = db.Column(db.String(2500), nullable=True)
+    # url = db.Column(db.String(2500), nullable=True)
+    def serialize(self):
+        return {
+            "id": self.id,
+            "title": self.name
+        }
+
+class Planets(db.Model):
+    __tablename__ = 'planets'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=True)
+    url = db.Column(db.String(2500), nullable=True)
+    climate = db.Column(db.String(250), nullable=True)
+    terrain = db.Column(db.String(250), nullable=True)
+    film_id= db.Column(db.Integer, ForeignKey('films.id'))
+    film = relationship(Films)
+
+class PlanetsInFilms(db.Model):
+    __tablename__ = 'planetsInFilms'
+    id = db.Column(db.Integer, primary_key=True)
+    planets_id = db.Column(db.Integer, ForeignKey('planets.id'))
+    planets = relationship(Planets)
+    film_id = db.Column(db.Integer, ForeignKey('films.id'))
+    film = relationship(Films)
 
 class Character(db.Model):
     __tablename__ = 'character'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
     id = db.Column(db.Integer, primary_key=True)
     film_id = db.Column(db.Integer, ForeignKey('films.id'))
     film = relationship(Films)
     people_id = db.Column(db.Integer, ForeignKey('people.id'))
     people = relationship(People)
 
-
-class People(Base):
-    __tablename__ = 'people'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(250), nullable=False)
-    hair_color = db.Column(db.String(30), nullable=False)
-    eye_color = db.Column(db.String(30), nullable=False)
-    birth_year = db.Column(db.Integer, nullable=False)
-    gender = db.Column(db.String(250), nullable=False)
-    # homeworld = db.Column(db.String(250), nullable=False)
-    # url = db.Column(db.String(250), nullable=False)
-
-
-# class Planets(Base):
-#     __tablename__ = 'planets'
-#     # Here we define columns for the table person
-#     # Notice that each column is also a normal Python instance attribute.
-#     id = Column(db.Integer, primary_key=True)
-#     name = Column(String(250), nullable=False)
-#     url = Column(String(2500), nullable=False)
-#     climate = Column(String(250), nullable=False)
-#     terrain = Column(String(250), nullable=False)
-
-#     # film_id= Column(db.Integer, ForeignKey('films.id'))
-#     # film = relationship(Films)
-
-#     # planets_id = Column(Integer, ForeignKey('films.id'))
-#     # planets = relationship(Films)
-
-# class PlanetsInFilms(Base):
-#     __tablename__ = 'planetsInFilms'
-#     id = Column(Integer, primary_key=True)
-#     planets_id = Column(Integer, ForeignKey('planets.id'))
-#     planets = relationship(Planets)
-#     film_id = Column(Integer, ForeignKey('films.id'))
-#     film = relationship(Films)
+class Character(db.Model):
+    __tablename__ = 'character'
+    id = Column(Integer, primary_key=True)
+    street_name = Column(String(250))
+    street_number = Column(String(250))
+    post_code = Column(String(250), nullable=True)
+    person_id = Column(Integer, ForeignKey('person.id'))
+    person = relationship(Person)
+    homeworld_id = Column(Integer, ForeignKey('planets.id'))
+    homeworld = relationship(Planets)
+    film_id = Column(Integer, ForeignKey('films.id'))
+    film = relationship(Films)
+    person_id = Column(Integer, ForeignKey('people.id'))
+    person = relationship(People)
 
 
-# class Vehicles(Base):
-#     __tablename__ = 'vehicles'
-#     # Here we define columns for the table person
-#     # Notice that each column is also a normal Python instance attribute.
-#     id = Column(Integer, primary_key=True)
-#     name = Column(String(250), nullable=False)
-#     url = Column(String(2500), nullable=False)
-#     model = Column(String(2500), nullable=False)
-#     vehicle_class = Column(String(2500), nullable=False)
-#     manufacturer = Column(String(2500), nullable=False)
-#     cost_in_credits = Column(String(2500), nullable=False)
-#     length = Column(Integer, nullable=False)
-#     crew = Column(String(2500), nullable=False)
-#     passangers = Column(Integer, nullable=False)
-#     max_atmosphering_speed = Column(Integer, nullable=False)
-#     cargo_capacity = Column(Integer, nullable=False)
-#     max_atmosphering_speed = Column(String(2500), nullable=False)
+class VehiclesInFilms(db.Model):
+    __tablename__ = 'vehiclesInFilms'
+    id = Column(Integer, primary_key=True)
+    vehicles_id = Column(Integer, ForeignKey('vehicles.id'))
+    vehicles = relationship(Vehicles)
+    film_id = Column(Integer, ForeignKey('films.id'))
+    film = relationship(Films)
 
 
-#     # film_id= Column(Integer, ForeignKey('films.id'))
-#     # film = relationship(Films)
-
-
-
-
-
-
-# class Address(Base):
-#     __tablename__ = 'address'
-#     # Here we define columns for the table address.
-# class Character(Base):
-#     __tablename__ = 'character'
-#     # Here we define columns for the table person
-#     # Notice that each column is also a normal Python instance attribute.
-#     id = Column(Integer, primary_key=True)
-#     street_name = Column(String(250))
-#     street_number = Column(String(250))
-#     post_code = Column(String(250), nullable=False)
-#     person_id = Column(Integer, ForeignKey('person.id'))
-#     person = relationship(Person)
-#     homeworld_id = Column(Integer, ForeignKey('planets.id'))
-#     homeworld = relationship(Planets)
-#     film_id = Column(Integer, ForeignKey('films.id'))
-#     film = relationship(Films)
-#     person_id = Column(Integer, ForeignKey('people.id'))
-#     person = relationship(People)
-
-
-# class VehiclesInFilms(Base):
-#     __tablename__ = 'vehiclesInFilms'
-#     id = Column(Integer, primary_key=True)
-#     vehicles_id = Column(Integer, ForeignKey('vehicles.id'))
-#     vehicles = relationship(Vehicles)
-#     film_id = Column(Integer, ForeignKey('films.id'))
-#     film = relationship(Films)
-
-
-# class Favorites(Base):
-#     __tablename__ = 'favorites'
-#     # Here we define columns for the table person
-#     # Notice that each column is also a normal Python instance attribute.
-#     id = Column(Integer, primary_key=True)
-#     username = Column(String(250), nullable=False)
-#     fav_planet_id = Column(Integer, ForeignKey('planets.id'))
-#     planet = relationship(Planets)
-#     fav_vehicle_id = Column(Integer, ForeignKey('vehicles.id'))
-#     vehicle = relationship(Films)
-#     fav_person_id = Column(Integer, ForeignKey('people.id'))
-#     person = relationship(People)
-
-
-
-
-    # def to_dict(self):
-    #     return {}
+class Favorites(db.Model):
+    __tablename__ = 'favorites'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(250), nullable=False)
+    fav_planet_id = Column(Integer, ForeignKey('planets.id'))
+    planet = relationship(Planets)
+    fav_vehicle_id = Column(Integer, ForeignKey('vehicles.id'))
+    vehicle = relationship(Films)
+    fav_person_id = Column(Integer, ForeignKey('people.id'))
+    person = relationship(People)
